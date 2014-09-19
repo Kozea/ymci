7c1faf749075b21da42c0b8d8c6fc60ba90da0b8
Florian Mounier <florian.mounier@kozea.fr>
2014-09-19 10:19:42 +0200

    Handle tox

--- plugins/ymci-ext-test-junit/ymci_ext_test_junit/db.py
+++ plugins/ymci-ext-test-junit/ymci_ext_test_junit/db.py
@@ -1,6 +1,7 @@
 from ymci.ext.db import Table
 from sqlalchemy import Column, Integer, ForeignKeyConstraint, String
 from sqlalchemy.orm import relationship, backref
+from sqlalchemy.ext.hybrid import hybrid_property
 
 
 class Result(Table):
@@ -12,17 +13,18 @@ class Result(Table):
     )
     project_id = Column(Integer, primary_key=True)
     build_id = Column(Integer, primary_key=True)
+    filename = Column(String, primary_key=True, server_default='results.xml')
 
     fail = Column(Integer)
     error = Column(Integer)
     skip = Column(Integer)
     total = Column(Integer)
 
-    @property
+    @hybrid_property
     def success(self):
         return self.total - self.fail - self.error
 
-    build = relationship('Build', backref=backref('result', uselist=False))
+    build = relationship('Build', backref='results')
 
 
 class JUnitConfig(Table):
--- plugins/ymci-ext-test-junit/ymci_ext_test_junit/routes.py
+++ plugins/ymci-ext-test-junit/ymci_ext_test_junit/routes.py
@@ -1,7 +1,9 @@
 from ymci.ext.routes import url, Route
-from ymci.model import Project
+from .db import Result
+from ymci.model import Project, Build
 from ymci import server
 from ymci.routes import graph_config
+from sqlalchemy import func
 import pygal
 
 
@@ -9,24 +11,36 @@ import pygal
 @url(r'/project/chart/results/(\d+)_(\d+)_(\d+).svg', suffix='Size')
 class ResultChart(Route):
     def get(self, id, width=None, height=None):
-        project = self.db.query(Project).get(id)
         config = graph_config(width, height)
         config.style.colors = ('#ff7156', '#ff4136', '#ff851b', '#28b62c')
         config.print_values = False
         svg = pygal.StackedBar(config)
-        builds = project.builds[::-1]
+        builds = (
+            self.db.query(
+                Build.build_id,
+                func.sum(Result.error).label('errors'),
+                func.sum(Result.fail).label('fails'),
+                func.sum(Result.skip).label('skips'),
+                func.sum(Result.success).label('success'))
+            .select_from(Build)
+            .join(Result, Build.results)
+            .filter(Build.project_id == id)
+            .group_by(Build.build_id)
+            .order_by(Build.build_id)
+            .all())
+
         svg.add('Errors', [{
             'xlink': self.reverse_url('ProjectLog', id, b.build_id),
-            'value': b.result.error if b.result else 0} for b in builds])
+            'value': b.errors} for b in builds])
         svg.add('Failures', [{
             'xlink': self.reverse_url('ProjectLog', id, b.build_id),
-            'value': b.result.fail if b.result else 0} for b in builds])
+            'value': b.fails} for b in builds])
         svg.add('Skips', [{
             'xlink': self.reverse_url('ProjectLog', id, b.build_id),
-            'value': b.result.skip if b.result else 0} for b in builds])
+            'value': b.skips} for b in builds])
         svg.add('Success', [{
             'xlink': self.reverse_url('ProjectLog', id, b.build_id),
-            'value': b.result.success if b.result else 0} for b in builds])
+            'value': b.success} for b in builds])
         if width and height:
             svg.x_labels = ['#%d' % b.build_id for b in builds]
 
