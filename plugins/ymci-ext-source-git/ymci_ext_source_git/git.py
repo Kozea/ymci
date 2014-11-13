320df5ef6c34615f1b92f9bedac031fe0cd8125a
Florian Mounier <florian.mounier@kozea.fr>
2014-11-13 16:06:36 +0100

    Support weird branch syntax in git

--- plugins/ymci-ext-source-git/ymci_ext_source_git/git.py
+++ plugins/ymci-ext-source-git/ymci_ext_source_git/git.py
@@ -12,11 +12,15 @@ class GitHook(BuildHook):
         return self.build.project.repository.startswith('git+')
 
     def pre_copy(self):
+        repo = self.build.project.repository[4:]
+        branch = 'master'
+        if '!' in repo:
+            repo, branch = repo.split('!')
         if not os.path.exists(
                 os.path.join(self.build.project.src_dir, '.git')):
             self.execute(
-                ['git', 'clone', self.build.project.repository[4:], '.'])
+                ['git', 'clone', repo, '.'])
 
         self.execute(['git', 'fetch', 'origin'])
         self.execute(
-            ['git', 'reset', '--hard', 'origin/master'])
+            ['git', 'reset', '--hard', 'origin/%s' % branch])
