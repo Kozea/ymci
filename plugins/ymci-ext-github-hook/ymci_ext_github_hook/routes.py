e88a4028e9359c9bc2d3e412ca7448ba4c8cf620
Florian Mounier <florian.mounier@kozea.fr>
2015-09-07 18:29:21 +0200

    Async github hook + no .git git url

--- plugins/ymci-ext-github-hook/ymci_ext_github_hook/routes.py
+++ plugins/ymci-ext-github-hook/ymci_ext_github_hook/routes.py
@@ -1,5 +1,6 @@
 from ymci.ext.routes import url, Route
 from ymci.model import Project, Build
+from ymci import ioloop
 from datetime import datetime
 import json
 
@@ -10,45 +11,52 @@ class GithubHook(Route):
         self.write('GET OK')
 
     def post(self):
-        hook = json.loads(self.request.body.decode('utf-8'))
-        if not hook.get('repository') or not hook.get('ref'):
-            self.write('VOID POST OK')
-            return
-        repos = [
-            hook['repository'].get('url'),
-            hook['repository'].get('ssh_url'),
-            hook['repository'].get('svn_url'),
-            hook['repository'].get('git_url'),
-            hook['repository'].get('clone_url')
-        ]
-        builds = []
+        data = self.request.body.decode('utf-8')
+        ioloop.spawn_callback(self.closure_for_treat(data))
+        self.write('Bye')
 
-        projects = self.db.query(Project).all()
-        for project in projects:
-            if project.repository.startswith('git+'):
-                repo = project.repository[4:]
-                branch = hook['repository'].get('default_branch', 'master')
-                if '!' in repo:
-                    repo, branch = repo.split('!')
-            else:
-                continue
+    def closure_for_treat(self, data):
+        def treat():
+            hook = json.loads(data)
+            if not hook.get('repository') or not hook.get('ref'):
+                return
+            repos = [
+                hook['repository'].get('url'),
+                hook['repository'].get('ssh_url'),
+                hook['repository'].get('ssh_url', '')[:-4],
+                hook['repository'].get('svn_url'),
+                hook['repository'].get('git_url'),
+                hook['repository'].get('git_url', '')[:-4],
+                hook['repository'].get('clone_url')
+            ]
+            builds = []
 
-            if repo and repo in repos:
-                if hook['ref'] == 'refs/heads/%s' % branch:
-                    build = Build()
-                    build.timestamp = datetime.now()
-                    build.build_id = (
-                        project.last_build and
-                        project.last_build.build_id or 0) + 1
-                    build.status = 'PENDING'
-                    project.builds.append(build)
+            projects = self.db.query(Project).all()
+            for project in projects:
+                if project.repository.startswith('git+'):
+                    repo = project.repository[4:]
+                    branch = hook['repository'].get('default_branch', 'master')
+                    if '!' in repo:
+                        repo, branch = repo.split('!')
+                else:
+                    continue
 
-                    self.db.add(build)
-                    builds.append(build)
+                if repo and repo in repos:
+                    if hook['ref'] == 'refs/heads/%s' % branch:
+                        build = Build()
+                        build.timestamp = datetime.now()
+                        build.build_id = (
+                            project.last_build and
+                            project.last_build.build_id or 0) + 1
+                        build.status = 'PENDING'
+                        project.builds.append(build)
 
-        self.db.commit()
+                        self.db.add(build)
+                        builds.append(build)
 
-        for build in builds:
-            self.application.builder.add(build)
+            self.db.commit()
 
-        self.write('POST OK')
+            for build in builds:
+                self.application.builder.add(build)
+
+        return treat
